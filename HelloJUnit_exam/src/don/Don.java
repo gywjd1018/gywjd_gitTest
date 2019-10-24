0b8241ace961365445a6abf9b246a397e0d952a2
gywjd <gywjd@DESKTOP-2NAQGM2>
2019-10-24 14:34:29 +0900

    commit

---
+++ HelloJUnit_exam/src/don/Don.java
@@ -0,0 +1,21 @@
+package don;
+
+class Kaja {
+
+	public static int kaja1(int mom, int daddy) {
+
+		return mom + daddy;
+	
+	}
+
+}
+
+public class Don {
+
+	public static void main(String [] args) {
+
+//		kaja(10000, 20000);
+	
+	}
+
+}
---
+++ HelloJUnit_exam/src/don/DonTest.java
@@ -0,0 +1,18 @@
+package don;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DonTest {
+
+	@Test
+	void testMain() {
+		
+//		fail("Not yet implemented");
+		
+		assertTrue(Kaja.kaja1(10000, 20000) == 30000);
+		
+	}
+
+}
